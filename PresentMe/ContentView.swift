14a993e2b796c8ab38419578689e07b10910d94b
reembojeer <reembojeer@gmail.com>
2023-10-08 11:33:37 +0300

    commit 1

--- PresentMe/ContentView.swift
+++ PresentMe/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  PresentMe
+//  precent me
 //
 //  Created by RIM ABDULLAH BUJEIR on 23/03/1445 AH.
 //
@@ -9,13 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+       
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            Image("reem")
+                .offset(y: -280)
+            
+            VStack{
+               
+                Image("d")
+                    .resizable()
+                    .scaledToFill()
+                       .frame(width: 300.0, height: 300.0)
+                
+                
+                Text("Reem Bojeer")
+                    .font(.largeTitle)
+                .frame(width: 300)
+                .foregroundStyle(.white)
+                Text("Web developer and UI/UX Designer from Jeddah")
+                    .frame(width: 300.0, height: 0)
+               
+                    
+                .frame(width: 100)
+                .foregroundStyle(.white)
+                
+                
+            }
+            
+            
+            
+          
+                   
+               
+           
         }
-        .padding()
+       
     }
 }
 
