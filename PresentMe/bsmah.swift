6204873944bf0e79e1eda0c0b3be5207d196e01b
somooo <somoooy@yahoo.com>
2023-10-08 13:01:45 +0300

    bsmah

---
+++ PresentMe/bsmah.swift
@@ -0,0 +1,18 @@
+//
+//  bsmah.swift
+//  PresentMe
+//
+//  Created by bsamh on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct bsmah: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    bsmah()
+}
