e6d7107def12cdda149168499db8299c4b03063d
reembojeer <reembojeer@gmail.com>
2023-10-08 11:28:33 +0300

    Initial Commit

---
+++ PresentMe/PresentMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentMeApp.swift
+//  PresentMe
+//
+//  Created by RIM ABDULLAH BUJEIR on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct PresentMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
