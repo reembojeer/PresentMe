90bc2d34eede2d9da676afe8c1662d7b234b14f9
GhaliahAltoom <ghaliah.altoom@outlook.com>
2023-10-08 12:01:28 +0300

    ff

--- PresentMe/SwiftUIView.swift
+++ PresentMe/SwiftUIView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    var body: some View { VStack {
+        Image("GhaliahImg")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.gray, lineWidth: 5))
+        
+        Text("Ghaliah Altoom")
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .bold()
+            .foregroundColor(.purple)
+            
+            
+            
+       
+        Text("Hello future developers. i'm fresh graduated programmer from Digital College and i'm trying to be UI designer. I love colors and arts that's whay i want to be UI designer.")
+            .font(.subheadline)
+        
+    }
     }
 }
 
