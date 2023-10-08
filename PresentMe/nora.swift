bd4ac018d699adb5074916310bbd2b041f04fc56
Norahalmukhlifi <norahalmokhlifi@gmail.com>
2023-10-08 12:59:25 +0300

    hhjh

---
+++ PresentMe/nora.swift
@@ -0,0 +1,18 @@
+//
+//  nora.swift
+//  PresentMe
+//
+//  Created by NorahAlmukhlifi on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct nora: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    nora()
+}
